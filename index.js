2b9178d475b0bfe492f1e7226dcb98b19f052786
mrKarton <gornostaev.dmitry04@gmail.com>
2021-01-25 20:46:38 +0400

    Lib Stylized

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
 var api = require('./lib');
 
-var maid = new api();
-
-
-var a = async () => {
-    await maid.connect('Example', 'http://127.0.0.1:8888');
-
-    maid.onMessage(async msg => {
-        console.log(msg);
-        if(msg.content == "ping")
-        {
-            await maid.send(msg.channelID, 'pong');
-        }
-    })
-}
-
-a();
+module.exports = api;
---
+++ test.js
@@ -0,0 +1,18 @@
+var api = require('./');
+
+var maid = new api();
+
+
+var a = async () => {
+    await maid.connect('Example', 'http://127.0.0.1:8888');
+
+    maid.onMessage(async msg => {
+        console.log(msg);
+        if(msg.content == "ping")
+        {
+            await maid.send(msg.channelID, 'pong');
+        }
+    })
+}
+
+a();
